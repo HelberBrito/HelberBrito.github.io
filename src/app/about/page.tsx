fa3e55fc522da6ad8fc168851b86e7d80e6e75b9
HB <helberbrito@gmail.com>
2023-11-01 12:58:33 -0300

    dev: cria rota "/about"

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1 class='mt-2'>
+        Você está em <strong>{'"SOBRE"'}</strong> <br></br>
+      </h1>
+      <h2 class='mt-20'>
+        devHB esteve aqui...!
+      </h2>
+    </div>
+  )
+}
